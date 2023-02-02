87710321f281977e3c9a76d20a8bcbd1d073021b
NNL1211 <nnhatlam1211@gmail.com>
2023-02-02 14:13:49 +0700

    update cors

--- server/index.js
+++ server/index.js
@@ -36,7 +36,7 @@ const server = app.listen(process.env.PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: "https://chat.nguyennhatlam.tech",
+    origin: "http://localhost:3000",
     credentials: true,
   },
 });
