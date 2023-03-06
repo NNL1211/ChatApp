81999bdcf8d6a5101396b10f314655355e4ba8ed
NNL1211 <nnhatlam1211@gmail.com>
2023-03-06 14:01:35 +0700

    update api domain

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,6 @@
-export const host = "https://apichat.nguyennhatlam.tech";
+/* A constant variable that is used to store the host address. */
+export const host = "http://chatapi.nguyennhatlam.tech";
+// export const host = "http://localhost:8001";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
