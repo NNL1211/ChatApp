b9b0fa5f5e0afa92caae89e463d19dc20d3b5291
NNL1211 <nnhatlam1211@gmail.com>
2023-03-07 14:53:31 +0700

    update cors domain

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,5 +1,5 @@
 /* A constant variable that is used to store the host address. */
-export const host = "http://chatapi.nguyennhatlam.tech";
+export const host = "https://chatapi.nguyennhatlam.tech";
 // export const host = "http://localhost:8001";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
